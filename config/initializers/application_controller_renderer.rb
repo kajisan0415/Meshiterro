d7e29c602610b739043d6a40303503ce6e5e7880
kajisan0415 <agonagakajisan0415@gmail.com>
2021-04-05 02:59:13 +0000

    Chapter3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9362fcf23fe8ea1da48b441d7d1bd7a97da6426787265e9ad8a5d2f3f57177061787dff191a130e86bf6ce9c1aa615c7aee4b421b29b81e55c8a9bfc8beac104'
